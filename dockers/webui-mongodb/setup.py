521ed13509a4a3ee3f45237b8166879e6c0adbac
root <root@ip-172-31-19-102.ap-northeast-1.compute.internal>
2016-06-06 10:20:05 +0000

    add git clone cmd in Dockerfile

--- dockers/webui-mongodb/setup.py
+++ dockers/webui-mongodb/setup.py
@@ -1,12 +1,15 @@
 import os
 import re
-webui_mongo_config = os.environ['WEBUI_MOGNO']+'/config.js'
+import socket
+
+webui_mongo_config = os.environ['WEBUI_MONGO']+'/config.js'
 
 db_name = 'verpix-dev-db'
 db_ip = '52.69.111.158'
 db_port = 27019
 user = 'user'
 passwd = 'pass'
+webui_ip = socket.gethostbyname(socket.gethostname())
 
 config_file = open(webui_mongo_config, 'r+')
 text = config_file.read()
@@ -20,4 +23,5 @@ config_file.seek(0)
 config_file.write(text)
 config_file.close()
 
+os.system('export VCAP_APP_HOST='+webui_ip)
 os.system('node '+os.environ['WEBUI_MOGNO']+'/app.js')
