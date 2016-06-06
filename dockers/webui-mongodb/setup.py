89f9897223d767c1e7c3580208ce4317ecb75bf0
root <root@ip-172-31-19-102.ap-northeast-1.compute.internal>
2016-06-06 09:20:18 +0000

    add dockers/webui-mongodb/*

---
+++ dockers/webui-mongodb/setup.py
@@ -0,0 +1,23 @@
+import os
+import re
+webui_mongo_config = os.environ['WEBUI_MOGNO']+'/config.js'
+
+db_name = 'verpix-dev-db'
+db_ip = '52.69.111.158'
+db_port = 27019
+user = 'user'
+passwd = 'pass'
+
+config_file = open(webui_mongo_config, 'r+')
+text = config_file.read()
+text = re.sub("\'db\'", "\'"+db_name+"\'", text)
+text = re.sub("\'localhost\'", "\'"+db_ip+"\'", text)
+text = re.sub('27017', str(db_port), text)
+text = re.sub("\'admin\'", "\'"+user+"\'", text)
+text = re.sub("\'pass\'", "\'"+passwd+"\'", text)
+
+config_file.seek(0)
+config_file.write(text)
+config_file.close()
+
+os.system('node '+os.environ['WEBUI_MOGNO']+'/app.js')
