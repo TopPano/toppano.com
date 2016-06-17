044016342b9fc36e54b34f8aabdea56c35c5835c
uniray7 <uniray7@gmail.com>
2016-06-17 07:59:21 +0000

    check the projects have been download install before

--- dockers/setup.py
+++ dockers/setup.py
@@ -1,6 +1,7 @@
 import subprocess
 from ConfigParser import SafeConfigParser
 from sys import stdin
+import os
 import getpass
 import yaml
 
@@ -11,6 +12,9 @@ def progress(op_code, cur_count, max_count=None, message=''):
 config_parser = SafeConfigParser()
 config_parser.read('configs')
 
+web_app_commit = config_parser.get('verpix-web-app', 'commit')
+web_app_port = int(config_parser.get('verpix-web-app', 'public_port'))
+
 laputa_api_commit = config_parser.get('laputa-api', 'commit')
 laputa_api_port = int(config_parser.get('laputa-api', 'public_port'))
 
@@ -20,36 +24,58 @@ laputa_db_commit = config_parser.get('laputa-db', 'commit')
 laputa_db_webui_port = int(config_parser.get('laputa-db', 'webui_public_port'))
 laputa_db_port = int(config_parser.get('laputa-db', 'db_public_port'))
 
+
 # clone all project
 # ask for github auth
 print("git clone laputa-api, verpix-async, laputa-db now")
 user = raw_input("Username for 'https://github.com':")
 passwd = getpass.getpass()
 
-HTTPS_REMOTE_URL = 'https://'+user+':'+passwd+'@github.com/TopPano/laputa-api.git'
+DEST_NAME = 'verpix-web-app'
+# check verpix-web-app is exist
+if not os.path.exists(DEST_NAME):
+    HTTPS_REMOTE_URL = 'https://'+user+':'+passwd+'@github.com/TopPano/verpix-web-app.git'
+    subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+
+if web_app_commit != '':
+    subprocess.call('cd '+DEST_NAME+' && git pull origin master && git checkout '+web_app_commit, shell=True)
+
+
 DEST_NAME = 'laputa-api'
-subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+# check laputa-api is exist
+if not os.path.exists(DEST_NAME):
+    HTTPS_REMOTE_URL = 'https://'+user+':'+passwd+'@github.com/TopPano/laputa-api.git'
+    subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+
 if laputa_api_commit != '':
-    subprocess.call('cd '+DEST_NAME+' && git checkout '+laputa_api_commit, shell=True)
+    subprocess.call('cd '+DEST_NAME+' && git pull origin master && git checkout '+laputa_api_commit, shell=True)
 
 
-HTTPS_REMOTE_URL = 'https://@github.com/TopPano/verpix-async.git'
 DEST_NAME = 'verpix-async'
-subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+# check verpix-async is exist
+if not os.path.exists(DEST_NAME):
+    HTTPS_REMOTE_URL = 'https://@github.com/TopPano/verpix-async.git'
+    subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+
 if verpix_async_commit != '':
-    subprocess.call('cd '+DEST_NAME+' && git checkout '+verpix_async_commit, shell=True)
+    subprocess.call('cd '+DEST_NAME+' && git pull origin master && git checkout '+verpix_async_commit, shell=True)
 
 
-HTTPS_REMOTE_URL = 'https://'+user+':'+passwd+'@github.com/TopPano/laputa-db.git'
 DEST_NAME = 'laputa-db'
-subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+# check laputa-db is exist
+if not os.path.exists(DEST_NAME):
+    HTTPS_REMOTE_URL = 'https://'+user+':'+passwd+'@github.com/TopPano/laputa-db.git'
+    subprocess.call(["git", "clone", HTTPS_REMOTE_URL, DEST_NAME])
+
 if laputa_db_commit != '':
-    subprocess.call('cd '+DEST_NAME+' && git checkout '+laputa_db_commit, shell=True)
+    subprocess.call('cd '+DEST_NAME+' && git pull origin master && git checkout '+laputa_db_commit, shell=True)
 
 
 # read docker-compose.yml
 r_stream = file('docker-compose_default.yml', 'r')
 docker_compose = yaml.load(r_stream)
+if isinstance(web_app_port, int):
+    docker_compose['services']['verpix-dev-web-app']['ports'] = [str(web_app_port)+':8000']
 if isinstance(laputa_api_port, int):
     docker_compose['services']['verpix-dev-laputa-api']['ports'] = [str(laputa_api_port)+':3000']
 if isinstance(laputa_db_webui_port, int):
