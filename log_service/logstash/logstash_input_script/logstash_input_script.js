22db4d7318f28244ac4ef79f0debcf4df3ab25b0
Ubuntu <ubuntu@ip-192-168-5-54.ap-northeast-1.compute.internal>
2016-07-14 07:49:03 +0000

    refactor logstash_input_script.js & add CPU Mem Utilization & 3 Statistics(Avg, Max, Min)

--- log_service/logstash/logstash_input_script/logstash_input_script.js
+++ log_service/logstash/logstash_input_script/logstash_input_script.js
@@ -2,8 +2,8 @@ var async = require('async');
 var AwsCli = require('aws-cli-js');
 
 var aws = new AwsCli({
-      aws_access_key_id: 'your key id', 
-      aws_secret_access_key: 'your secret key',
+      aws_access_key_id: 'ur key id', 
+      aws_secret_access_key: 'ur secret key',
 });
 
 
@@ -11,18 +11,14 @@ var region = 'ap-northeast-1';
 var tag = 'production';
 var filters = 'Name=tag:Env,Values='+tag;
 
-var metrics = ['CPUUtilization'];
-var namespace = ['AWS/EC2'];
+var metrics = [{'namespace': 'AWS/EC2', 'metric':'CPUUtilization', 'statistics': ['Average', 'Maximum', 'Minimum']},
+               {'namespace': 'System/Linux', 'metric':'MemoryUtilization', 'statistics': ['Average', 'Maximum', 'Minimum']} ];
 
 
-var statistics = ['Average'];
-
-
-process.env.TZ = 'Europe/Amsterdam' 
 // ==== get current timestamp in Taiwan timezone
-function getCurTs(callback){
+function getCurTs(){
     var curr_timestamp = new Date();
-    callback(null, curr_timestamp.toISOString());
+    return curr_timestamp.toISOString();
 }
 
 
@@ -31,13 +27,27 @@ function getInstanceId(args, callback){
     aws.command('ec2 describe-instances --region '+args.region+' --filters '+args.filters).
         then(function (data) {
             var reserv = data.object.Reservations;
-            var insts;
-            var reserv_idx, inst_idx;
+            var insts, tags;
+            var reserv_idx, inst_idx, tag_idx;
             var result = [];
+
+            var refactored_inst;
+            // the received data contains 4 object because of 4 security group
+            // each group havs their own instances
             for (reserv_idx = 0; reserv_idx < reserv.length; reserv_idx++){
                 insts = reserv[reserv_idx].Instances;
                 for(inst_idx = 0; inst_idx < insts.length; inst_idx++){
-    	    	    result.push(insts[inst_idx].InstanceId);
+                    refactored_inst = {};
+                    refactored_inst.instanceId = insts[inst_idx].InstanceId;
+                    refactored_inst.volumeId = insts[inst_idx].BlockDeviceMappings[0].Ebs.VolumeId;
+                 
+                    tags = insts[inst_idx].Tags;
+                    refactored_inst.tags = {};
+                    for(tag_idx = 0; tag_idx < tags.length; tag_idx++)
+                    {
+                        refactored_inst.tags[tags[tag_idx].Key] = tags[tag_idx].Value;
+                    }
+                    result.push(refactored_inst);
                 }
             }
             callback(null, result);
@@ -45,36 +55,51 @@ function getInstanceId(args, callback){
 }
 
 
-
 // ==== request for metrics of instances
-function getMetrics(currTs, instanceIds){
-    var d = new Date(currTs);
-    var utc = d.getTime();
+function getMetrics(instance_arr, callback){
+    var currTs = new Date();
+    var startTs = new Date();
+    
     // sub 15 minutes
-    utc = utc - 900000;
-    var startTs = new Date(utc).toISOString();
-    var result = [];
+    startTs.setMinutes(startTs.getMinutes()-15);
+    currTs = currTs.toISOString();
+    startTs = startTs.toISOString();
 
+    var result = [];
     var aws_q = async.queue(function (task, callback) {
-            aws.command('cloudwatch get-metric-statistics --metric-name '+task.metric+' --start-time '+startTs+' --end-time '+currTs+' --period 300 --region '+region+' --namespace '+task.namespace+' --statistics Average --dimensions Name=InstanceId,Value='+task.instanceId).then(
+        aws.command('cloudwatch get-metric-statistics --metric-name '+task.metric+' --start-time '+startTs+' --end-time '+currTs+' --period 300 --region '+region+' --namespace '+task.namespace+' --statistics '+task.statistic+' --dimensions Name=InstanceId,Value='+task.instance.instanceId).then(
                 function(data){
                     var tmp;
                     var j;
                     for(j=0; j<data.object.Datapoints.length; j++){
                         tmp = {};  
-                        tmp['instanceId'] = task.instanceId;
-                        tmp['metric'] = data.object.Datapoints[j];
-                        tmp['metric']["type"] = task.metric;
+                        tmp['target'] = task.instance;
+                        tmp['metric'] = {};
+                        tmp['metric']["Timestamp"] = data.object.Datapoints[j]["Timestamp"];
+                        tmp['metric']["Unit"] = data.object.Datapoints[j]["Unit"];
+                        tmp['metric']["Type"] = task.metric;
+                        tmp['metric']["Statistic"] = task.statistic;
+                        tmp['metric']["Value"] = data.object.Datapoints[j][task.statistic];
                         result.push(tmp);
                     }
                     callback()
                 });
     }, 5);
 
-    async.each(instanceIds, 
-            function(instanceId){
-                aws_q.push({instanceId: instanceId, metric:"CPUUtilization", namespace:"AWS/EC2"}, function(){/*console.log("finish "+instanceId);*/});
-                aws_q.push({instanceId: instanceId, metric:"MemoryUtilization", namespace:"System/Linux"}, function(){/*console.log("finish "+instanceId);*/});
+    async.each(instance_arr, 
+            function(instance){
+                var i, j;
+                for(i = 0; i<metrics.length; i++){
+                    for(j = 0; j<metrics[i].statistics.length; j++){
+                        aws_q.push({
+                                instance: instance, 
+                                metric: metrics[i].metric,
+                                statistic: metrics[i].statistics[j], 
+                                namespace: metrics[i].namespace}, 
+                            function(){/*console.log("finish "+instanceId);*/});
+                    }
+                }
+                //aws_q.push({instanceId: instanceId, metric:"MemoryUtilization", namespace:"System/Linux"}, function(){/*console.log("finish "+instanceId);*/});
             }, 
             function(err){console.log(err);})
 
@@ -85,14 +110,6 @@ function getMetrics(currTs, instanceIds){
 
 
 
-async.parallel([
-        getCurTs, 
-        getInstanceId.bind(null, {"region":region, "filters": filters})], 
-    function(err, res){
-        getMetrics(res[0], res[1]);
-    });
+var main = async.seq(getInstanceId, getMetrics);
 
-
-
-
-
+main({"region":region, "filters": filters}, function(){});
